d2ff4fb49f6b5b6f32d6877fbc865cc637ca80ae
mahi-saxena27 <mahisaxena2704@gmail.com>
2025-04-09 09:37:01 +0530

    Add files via upload

---
+++ PaymentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp1.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Process(string cardNumber,string name,string expiry,string cvv)
+        {
+            if(string.IsNullOrEmpty(cardNumber)||string.IsNullOrEmpty(name))
+            {
+                ViewBag.Error = "Payment Failed:Invalid details.";
+                return View("Index");  
+            }
+            return RedirectToAction("ThankYou", "Order");
+        }
+    }
+}
